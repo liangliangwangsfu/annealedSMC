eb451d8c2e006ed2b31fc81befe727d2942d33f1
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2017-06-13 16:11:49 +0100

    Fix missing parser, add harder HMM example

--- src/main/java/mains/HMM.java
+++ src/main/java/mains/HMM.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.jgrapht.UndirectedGraph;
 
+import bayonet.distributions.Multinomial;
 import bayonet.distributions.Random;
 import bayonet.graphs.GraphUtils;
 import bayonet.marginal.DiscreteFactorGraph;
@@ -32,6 +33,9 @@ public class HMM implements Model<AnnealedLikelihoodParticle<List<Integer>>>
   
   @Arg               @DefaultValue("1")
   public Random random = new Random(1);
+  
+  @Arg                       @DefaultValue("false")
+  public boolean setAllObservationsToZero = false;
   
   @Override
   public AnnealingKernels<AnnealedLikelihoodParticle<List<Integer>>> kernels()
@@ -195,13 +199,54 @@ public class HMM implements Model<AnnealedLikelihoodParticle<List<Integer>>>
     }
   }
   
-  @Implementations({SimpleThreeStates.class, SimpleTwoStates.class})
+  @Implementations({SimpleThreeStates.class, SimpleTwoStates.class, LowPrValley.class})
   static interface Parameters
   {
     double [][] transitionPrs();
     double [][] emissionPrs();
     double [] initialPrs();
   } 
+  
+  public static class LowPrValley implements Parameters
+  {
+    @Arg     @DefaultValue("0.01")
+    public double epsilon = 0.01;
+
+    @Override
+    public double[][] transitionPrs()
+    {
+      double [][] result = new double[5][5];
+      for (int i = 0; i < 5; i++)
+        for (int j = 0; j < 5; j++)
+          if (Math.abs(i - j) < 2)
+            result[i][j] = 1.0;
+      for (int r = 0; r < 5; r++)
+        Multinomial.normalize(result[r]);
+      return result;
+    }
+
+    @Override
+    public double[][] emissionPrs()
+    {
+      double [][] result = new double[5][];
+      if (epsilon >= 1.0)
+        throw new IllegalArgumentException();
+      result[0] = result[3] = new double[]{0.2, 0.8};
+      result[1] = new double[]{0.1, 0.9};
+      result[4] = new double[]{0.3, 0.7};
+      result[3] = new double[]{epsilon, 1.0 - epsilon};
+      return result;
+    }
+
+    @Override
+    public double[] initialPrs()
+    {
+      double [] result = new double[5];
+      for (int i = 0; i < 5; i++)
+        result[i] = 1.0/5.0;
+      return result;
+    }
+  }
   
   public static class SimpleThreeStates implements Parameters
   {
